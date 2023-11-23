b5370891a0354788460ed783300565fe733c68db
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-23 12:03:53 +0300

    Declared a function to add an item to the saved items state

--- src/App.js
+++ src/App.js
@@ -37,6 +37,28 @@ function App()
       .then(response => response.json())
       .then(savedItems => setCart(savedItems))
   },[])
+
+  //Function to add item to saved items
+  const addItemToSaved = id =>
+  {
+    const savedProduct=products.find(product => product.id === parseInt(id))
+
+    //Making a POST request to the server
+    fetch("https://phase-2-ecommerce-project-api.onrender.com/savedItems",
+    {
+      method: "POST",
+      headers: 
+      {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(savedProduct)
+    })
+    .then(alert("Item has been added to your saved items"))
+
+    //Updating the saved items state
+    setSavedItems([...savedItems, savedProduct])
+  }
+  
   return (
     <>
       
