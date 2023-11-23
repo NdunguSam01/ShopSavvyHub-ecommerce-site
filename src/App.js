84d91ccbd6450655126daace414bf570e6abdabe
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-11-23 12:02:30 +0300

    Declared the states needed for this project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import './css/CardBody.css'
 import './css/Item.css'
 import './css/Navbar.css'
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() 
+{
+  //Declaring the states to be used in this project
+  const [products, setProducts]=useState([])
+  const [cart, setCart]=useState([])
+  const [savedItems, setSavedItems]=useState([])
+
+  
   return (
     <>
       
